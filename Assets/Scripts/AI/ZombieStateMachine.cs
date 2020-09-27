87bd1afed217353d8722c5cd49a910532dfc9423
gamedevgurufiverr <70965110+gamedevgurufiverr@users.noreply.github.com>
2020-09-27 18:20:42 +0100

    added zombie

---
+++ Assets/Scripts/AI/ZombieStateMachine.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieStateMachine : MonoBehaviour
+{
+    public enum State{
+        WANDER,
+        MUTATE,
+        CHASE
+    }
+
+    public State m_curr_state;
+    public Stack<State> m_curr_state_stack = new Stack<State>();
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update() {
+        UpdateAI();
+    }
+
+    private void UpdateAI()
+    {
+        switch(m_curr_state)
+        {
+            case State.WANDER:
+            {
+                //If we have reached our waypoint
+                if (HasReachedWaypoint())
+                {
+                    //Grab a new waypoint
+                    FindNewRandomWaypoint();
+                }
+                break;
+            }
+            case State.MUTATE:
+            {
+                break;
+            }
+            case State.CHASE:
+            {
+                
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
+    private bool HasReachedWaypoint()
+    {
+        if (Vector3.Distance(gameObject.GetComponent<NavMeshAgent>().destination, gameObject.transform.position) <= 0.1f)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void FindNewRandomWaypoint()
+    {
+        Vector3 random_pos = UnityEngine.Random.insideUnitySphere * 10;
+
+        FindPathToTarget(random_pos);
+    }
+
+    private void FindPathToTarget(Vector3 target)
+    {
+        NavMeshPath path = new NavMeshPath();
+
+        gameObject.GetComponent<NavMeshAgent>().CalculatePath(target, path);
+        gameObject.GetComponent<NavMeshAgent>().SetPath(path);
+    }
+
+}
